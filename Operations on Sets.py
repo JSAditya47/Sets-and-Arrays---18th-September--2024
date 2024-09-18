f60ac3344dfd2ce7cb085deb76bf0c42f1818e4d
Jyotirmoy Saha Aditya <adityajyotirmoysaha@gmail.com>
2024-09-18 09:53:51 +0600

    Nice

---
+++ Operations on Sets.py
@@ -0,0 +1,16 @@
+mySet = {1, 2, 3}
+print(mySet)
+
+mySet2 = {1.0, "Hello", (1, 2, 3)}
+print(mySet2)
+
+li = ["Farhan", "Aditya", "Nahian"]
+setList = set(li)
+print(setList)
+
+setList.add("Giga")
+print(setList)
+
+setList.pop()
+print(setList)
+
