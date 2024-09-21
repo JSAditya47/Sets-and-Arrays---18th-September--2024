4724fb25687a9aa6a89aa5c741ee9ae70781c35d
Jyotirmoy Saha Aditya <adityajyotirmoysaha@gmail.com>
2024-09-21 12:20:38 +0600

    TBH, ACP FR

---
+++ Set Symmmetric Difference.py
@@ -0,0 +1,12 @@
+set1 = {'green', 'red'}
+set2 = {'green', 'orange'}
+
+newSet1 = set1.symmetric_difference(set2)
+print(newSet1)
+
+set3 = {1, 3, 5, 7, 8, 10}
+set4 = {3, 5, 9, 2, 7, 10, 2, 4}
+
+newSet2 = set3.symmetric_difference(set4)
+print(newSet2)
+    
